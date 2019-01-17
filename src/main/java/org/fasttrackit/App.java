5c6a6705a4aa3672a2b0e9a51d4da8fbd661e413
Sandor Emanuel <sandor_emi@yahoo.com>
2019-01-17 19:39:35 +0200

    studied arrays

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,7 +1,5 @@
 package org.fasttrackit;
 
-import org.fasttrackit.cheater.UFO;
-
 /**
  * o clasa poate sa extinda o singura clasa
  * orice clasa mosteneste implicit clasa object
@@ -10,68 +8,71 @@ import org.fasttrackit.cheater.UFO;
  */
 public class App {
     public static void main(String[] args) {
-        Engine engine = new Engine();
-        engine.manufacturer = "Renault";
-        engine.capacity = 1500;
-
-        Car car = new Car(engine); //contructor "=" se ocupa de crearea obiectului respectiv
-        car.setName("Dacia");
-        car.setColor("White");
-        car.doorCount = 5;
-        car.setMileage(7.5);
-        car.running = true;
-        System.out.println(car);
-
-
-//        Engine engine1 = new Engine();
-//        Car car1 = new Car(engine1);
+//        Engine engine = new Engine();
+//        engine.manufacturer = "Renault";
+//        engine.capacity = 1500;
+//
+//        Car car = new Car(engine); //contructor "=" se ocupa de crearea obiectului respectiv
+//        car.setName("Dacia");
+//        car.setColor("White");
+//        car.doorCount = 5;
+//        car.setMileage(7.5);
+//        car.running = true;
+//        System.out.println(car);
 //
 //
-//        System.out.println(car.getName());
+////        Engine engine1 = new Engine();
+////        Car car1 = new Car(engine1);
+////
+////
+////        System.out.println(car.getName());
+////
+////
+////        /**facem masina (car) sa accelereze
+////         * stocam distanta intr-o variabila double carTravelDistance
+////         * car.accelerate - actionam masina cu anumiti parametri/obiectul car acceseaza metoda accelerate la parametrii definiti
+////         */
+////        double carTravelDistance = car.accelerate(100, 2);
+////
+////
+////        System.out.println(carTravelDistance);
+////
+////
+////        car.engine.manufacturer = "BMW";
+////        System.out.println(car.toString());
+//
+//        //example of static variables (class) unique for the whole aplicattion
+//        Vehicle vehicle1 = new Vehicle();
+//        vehicle1.vehicleCount ++;
+//
+//        Vehicle vehicle2 = new Vehicle();
+//        vehicle2.vehicleCount ++;
+//
+//        Vehicle vehicle3 = new Vehicle();
 //
 //
-//        /**facem masina (car) sa accelereze
-//         * stocam distanta intr-o variabila double carTravelDistance
-//         * car.accelerate - actionam masina cu anumiti parametri/obiectul car acceseaza metoda accelerate la parametrii definiti
-//         */
-//        double carTravelDistance = car.accelerate(100, 2);
+//        System.out.println("Value from vehicle 1: "+vehicle1.vehicleCount);
+//        System.out.println("Value from vehicle 2: "+vehicle2.vehicleCount);
+//        System.out.println("Value from Vehicle CLASS: "+Vehicle.vehicleCount);
 //
+//        new AutoVehicle();
+//        new AutoVehicle(new Engine());
 //
-//        System.out.println(carTravelDistance);
+//        vehicle1.accelerate(34.12);
+//        vehicle2.accelerate(100, 2);
 //
+//        new UFO().accelerate(100, 2);
 //
-//        car.engine.manufacturer = "BMW";
-//        System.out.println(car.toString());
+//        //Polymorphysm (an object can take multple forms)
+//        Vehicle ufo = new UFO();
+//        //Type of the variable does not determine object behavior
+//        ufo.accelerate(200,2);
+//        //The type of the variable determines what methods can be invoked
+//        //we migh need type casting (see below)
+//        ((UFO) ufo).concealCheating();
 
-        //example of static variables (class) unique for the whole aplicattion
-        Vehicle vehicle1 = new Vehicle();
-        vehicle1.vehicleCount ++;
-
-        Vehicle vehicle2 = new Vehicle();
-        vehicle2.vehicleCount ++;
-
-        Vehicle vehicle3 = new Vehicle();
-
-
-        System.out.println("Value from vehicle 1: "+vehicle1.vehicleCount);
-        System.out.println("Value from vehicle 2: "+vehicle2.vehicleCount);
-        System.out.println("Value from Vehicle CLASS: "+Vehicle.vehicleCount);
-
-        new AutoVehicle();
-        new AutoVehicle(new Engine());
-
-        vehicle1.accelerate(34.12);
-        vehicle2.accelerate(100, 2);
-
-        new UFO().accelerate(100, 2);
-
-        //Polymorphysm (an object can take multple forms)
-        Vehicle ufo = new UFO();
-        //Type of the variable does not determine object behavior
-        ufo.accelerate(200,2);
-        //The type of the variable determines what methods can be invoked
-        //we migh need type casting (see below)
-        ((UFO) ufo).concealCheating();
+        Game game = new Game();
+        game.start();
 
 
 
--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -2,6 +2,32 @@ package org.fasttrackit;
 
 public class Game {
 
+    // crearea unui sir de circuite, trebuie specicat cate circuite dorim. (10)
+    private Track[] tracks = new Track[10];
+
     Vehicle firstCompetitor;
     Vehicle secondCompetitor;
+
+    public void start(){
+        addTracks();
+        displayAvailableTracks();
+    }
+
+    //facem o metoda prin care sa accesam toate track-urile din aplicatie
+    private void addTracks(){
+        Track track1 = new Track("Highway", 300);
+        Track track2 = new Track("Seaside", 100);
+
+        tracks[0]= track1;
+        tracks[1]= track2;
+
+    }
+
+    private void displayAvailableTracks(){
+        System.out.println("Available tracks:");
+        System.out.println(tracks[0].getName());
+        System.out.println(tracks[1].getName());
+
+    }
+
 }
